5d7e69b27eba71ff7a6edc6f09d455079b2f7475
cseltol <64264529+cseltol@users.noreply.github.com>
2021-10-07 21:13:12 +0700

    fix: remove SECRET_KEY

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!rja2r8%4w)$jaw^%6+!0w(32#px20=kv7z)_1zwcao=zyqec8'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
